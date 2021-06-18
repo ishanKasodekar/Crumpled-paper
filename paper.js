49c3cb1cc7b1121f46d6f9faac0a475b3ed7ab63
ishanKasodekar <76422610+ishanKasodekar@users.noreply.github.com>
2021-06-18 13:56:49 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,26 @@
+class Paper{
+    constructor(x,y,radius){
+        var options ={
+            isStatic:false,
+            'restitution':0.3 ,
+            'density':1.2 ,
+            'friction':0.5 
+        }
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.image=loadImage("paper.png");
+        this.body = Bodies.circle(this.x,this.y,(this.radius-20)/2,options);
+        World.add(world,this.body);
+    }
+    display(){
+    var pos=this.body.position;
+    push();
+    translate(pos.x,pos.y);
+    imageMode(CENTER);
+    image(this.image,0,0,this.radius,this.radius);
+    pop();
+    
+    
+    }
+}
